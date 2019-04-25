fb4d3796407df2665e50966f00f763896565d855
Vinicius Oliveira <vinicius.developer@outlook.com>
2019-04-24 21:56:05 -0300

    Atualizações de try

--- src/br/com/digitalhouse/Main.java
+++ src/br/com/digitalhouse/Main.java
@@ -18,64 +18,70 @@ public class Main {
         System.out.println("Seja Bem vinda  a Livraria");
         System.out.println("Digite o código da ação desejada:");
         System.out.println("1 - Cadastro de Livro\n2 - Consulta de Livro\n3 - Venda de Livro");
-        acao = opcao.nextInt();
-
-        switch (acao) {
-            case 1:
-                try {
-                    Livro livro = new Livro();
-                    Scanner valor = new Scanner(System.in);
-                    System.out.println("Cadastro de Livro\n Digite o nome do seu livro:");
-                    livro.setTitulo(valor.next());
-                    System.out.println("Digite o nome do Autor:");
-                    livro.setAutor(valor.next());
-                    System.out.println("Digite o Ano de Lançamento:");
-                    livro.setAnoLancemento(valor.nextInt());
-                    System.out.println("Digite o código ISBN:");
-                    livro.setISBN(valor.next());
-                    System.out.println("Digita o código do Livro");
-                    livro.setCodigo(valor.nextInt());
-                    System.out.println("Digita o preço:");
-                    livro.setPreco(valor.nextDouble());
-                    System.out.println("Digita a quantidade:");
-                    livro.setQuantidade(valor.nextInt());
-                    if (livraria.adicionarLivro(livro)) {
-                        System.out.println("Cadastrado com sucesso o seguinte livro:\n" + livro.toString());
-                    } else {
-                        System.out.println("Houve algum erro, reinicie o processo!");
+        try{
+            acao = opcao.nextInt();
+            switch (acao) {
+                case 1:
+                    try {
+                        Livro livro = new Livro();
+                        Scanner valor = new Scanner(System.in);
+                        System.out.println("Cadastro de Livro\n Digite o nome do seu livro:");
+                        livro.setTitulo(valor.next());
+                        System.out.println("Digite o nome do Autor:");
+                        livro.setAutor(valor.next());
+                        System.out.println("Digite o Ano de Lançamento:");
+                        livro.setAnoLancemento(valor.nextInt());
+                        System.out.println("Digite o código ISBN:");
+                        livro.setISBN(valor.next());
+                        System.out.println("Digita o código do Livro");
+                        livro.setCodigo(valor.nextInt());
+                        System.out.println("Digita o preço:");
+                        livro.setPreco(valor.nextDouble());
+                        System.out.println("Digita a quantidade:");
+                        livro.setQuantidade(valor.nextInt());
+                        if (livraria.adicionarLivro(livro)) {
+                            System.out.println("Cadastrado com sucesso o seguinte livro:\n" + livro.toString());
+                        } else {
+                            System.out.println("Houve algum erro, reinicie o processo!");
+                        }
+                    }catch (Exception ex){
+                        System.out.println("Valor incorreto. Tente novamente");
                     }
-                }catch (Exception ex){
-                    System.out.println("Valor incorreto. Tente novamente");
-                }
-                break;
-            case 2:
-                System.out.println("Digita o código do livro cadastro:");
-                int codigo;
-                Scanner valor2 = new Scanner(System.in);
-                codigo = valor2.nextInt();
-                System.out.println(livraria.consultarLivro(codigo));
-                break;
-            case 3:
-                List<Livro> totais = livraria.getLivroCadastrado();
-                for (Livro item : totais
-                ) {
-                    System.out.println(item.getCodigo() + " - " + item.getTitulo());
-                }
-                System.out.println("Com base na lista acima escolha um livro para a venda:");
-                Scanner valor3 = new Scanner(System.in);
+                    break;
+                case 2:
+                    System.out.println("Digita o código do livro cadastro:");
+                    int codigo;
+                    Scanner valor2 = new Scanner(System.in);
+                    codigo = valor2.nextInt();
+                    System.out.println(livraria.consultarLivro(codigo));
+                    break;
+                case 3:
+                    List<Livro> totais = livraria.getLivroCadastrado();
+                    for (Livro item : totais
+                    ) {
+                        System.out.println(item.getCodigo() + " - " + item.getTitulo());
+                    }
+                    System.out.println("Com base na lista acima escolha um livro para a venda:");
+                    Scanner valor3 = new Scanner(System.in);
 
-                try {
-                    int livroCodigo;
-                    livroCodigo = valor3.nextInt();
-                    System.out.println(livraria.efetuarVenda(livroCodigo));
-                } catch (Exception ex) {
-                    System.out.println("Opção Invalida campeão! Tenta de novo");
-                }
+                    try {
+                        int livroCodigo;
+                        livroCodigo = valor3.nextInt();
+                        System.out.println(livraria.efetuarVenda(livroCodigo));
+                    } catch (Exception ex) {
+                        System.out.println("Opção Invalida campeão! Tenta de novo");
+                    }
 
-                break;
-            default:
-                System.out.println("Opção Invalida.");
+                    break;
+                default:
+                    System.out.println("Opção Invalida.");
+            }
+        }catch (Exception ex){
+            System.out.println("Digite uma opção certa ai meo!");
         }
+
+
+
 
 
     }
